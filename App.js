c78aa7fdc0c66c7682db80ca189b2e61a23fcb66
RetroRanger444 <vignesh.s.blr@gmail.com>
2025-06-10 20:10:50 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,11 +5,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 
-import SettingsScreen from './screens/SettingsScreen';
-import AppearanceScreen from './screens/AppearanceScreen';
-import DeveloperOptionsScreen from './screens/DeveloperOptionsScreen';
-import ImportExportScreen from './screens/ImportExportScreen';
-import NotificationsScreen from './screens/NotificationsScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
+import AppearanceScreen from './src/screens/AppearanceScreen';
+import DeveloperOptionsScreen from './src/screens/DeveloperOptionsScreen';
+import ImportExportScreen from './src/screens/ImportExportScreen';
+import NotificationsScreen from './src/screens/NotificationsScreen';
 
 
 const Stack = createNativeStackNavigator();
